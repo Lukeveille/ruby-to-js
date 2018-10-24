d9c06a03f67795c1037d707c7a6e2c9402fc1a0a
Lukeveille <luke@lukeleveille.com>
2018-10-24 10:32:27 -0400

    completed part 1 and 2

---
+++ ruby-to-js.js
@@ -0,0 +1,15 @@
+// 1
+var apples = 14;
+console.log(apples);
+
+console.log("I have " + apples + " apples.");
+
+// 2
+var materials = ['wood', 'metal', 'stone'];
+words = {
+  'elephant': "The world's largest land mammal.",
+  'school': 'A place of learning.',
+  'ice cream': 'A delicious milk-based dessert.'
+}
+
+console.log(words);
